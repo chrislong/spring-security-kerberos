e1246d6086b0c96f2bc7ef043ece1435993697e7
Chris Long <chris.long@nextcentury.com>
2019-01-08 14:59:37 -0500

    fixing bug introduced by merge; version -> 1.0.5

--- spring-security-kerberos-samples/sec-server-win-auth/src/main/java/demo/app/WebSecurityConfig.java
+++ spring-security-kerberos-samples/sec-server-win-auth/src/main/java/demo/app/WebSecurityConfig.java
@@ -44,12 +44,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Value("${app.ldap-search-filter}")
 	private String ldapSearchFilter;
 
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
